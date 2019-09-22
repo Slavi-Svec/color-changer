35744c527e52807733e892e7126d3a506b41cf86
Slavi-Svec <slavi.svec@gmail.com>
2019-09-22 19:47:29 +1000

    Update color

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const colorBtn = document.querySelector('.colorBtn');
 const bodyBg = document.querySelector('body');
+const colorBtn2 = document.querySelector('.colorBtn');
 
 const colors = ['yellow','green','blue','orange','pink','majenta','cyan','violet'];
 
 colorBtn.addEventListener('click',changeColor);
 
-function changeColor (){
-
-    let random = Math.floor(Math.random()*colors.length)
+function changeColor() {
+    let random = Math.floor(Math.random() * colors.length)
+    let randomTwo = Math.floor(Math.random() * colors.length)
     bodyBg.style.backgroundColor = colors[random];
+    colorBtn2.style.backgroundColor = colors[randomTwo];
 }
-
-
